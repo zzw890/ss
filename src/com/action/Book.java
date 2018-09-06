4d7a3f645478b745d92b40831688df5718e47b8e
zhongzhiwei <zhongzhiwei>
2018-09-06 18:01:27 +0800

    提交修改

--- src/com/action/Book.java
+++ src/com/action/Book.java
@@ -13,7 +13,7 @@ import java.util.Date;
 public class Book extends HttpServlet {
 	private BookDAO bookDAO = null;
 
-	public Book() {666666666666666666666688888888888899999258
+	public Book() {666666666666666666666688888888888899999258789
 		this.bookDAO = new BookDAO();//fhhgfhgfbvcbcvvxvcxvxc zzw123
 	}
 
@@ -22,7 +22,7 @@ public class Book extends HttpServlet {
 		String action = request.getParameter("action");
 		System.out.println("\nbook*********************action=" + action);
 		if (action == null || "".equals(action)) {
-			request.setAttribute("error", "淇℃伅閿欒");
+			request.setAttribute("error", "娣団剝浼呴柨娆掝嚖");
 			request.getRequestDispatcher("error.jsp")
 					.forward(request, response);
 		} else if ("bookAdd".equals(action)) {
@@ -42,7 +42,7 @@ public class Book extends HttpServlet {
 		}
 	}
 
-	/*********************** 閿熸枻鎷烽敓閰电》鎷烽敓鏂ゆ嫹閿熻緝锟� **************************/
+	/*********************** 闁跨喐鏋婚幏鐑芥晸闁扮數銆嬮幏鐑芥晸閺傘倖瀚归柨鐔荤窛閿燂拷 **************************/
 	private void bookAdd(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		BookForm bookForm = new BookForm();
@@ -56,7 +56,7 @@ public class Book extends HttpServlet {
 		bookForm.setPage(request.getParameter("page")==""?0:Integer.parseInt(request.getParameter("page")));
 		bookForm.setBookcaseid(Integer.parseInt(request
 				.getParameter("bookcaseid")));
-		// 閿熸枻鎷峰彇绯荤粺閿熸枻鎷烽敓鏂ゆ嫹
+		// 闁跨喐鏋婚幏宄板絿缁崵绮洪柨鐔告灮閹风兘鏁撻弬銈嗗
 		Date date1 = new Date();
 		java.sql.Date date = new java.sql.Date(date1.getTime());
 		bookForm.setInTime(date.toString());
@@ -65,25 +65,25 @@ public class Book extends HttpServlet {
 		if (a == 1) {
 			request.getRequestDispatcher("book_ok.jsp?para=1").forward(request, response);
 		} else if (a == 2) {
-			request.setAttribute("error", "閿熸枻鎷峰浘閿熸枻鎷烽敓鏂ゆ嫹鎭敓绐栨拝鎷烽敓鏂ゆ嫹妯遍敓锟�");
+			request.setAttribute("error", "闁跨喐鏋婚幏宄版禈闁跨喐鏋婚幏鐑芥晸閺傘倖瀚归幁顖炴晸缁愭牗鎷濋幏鐑芥晸閺傘倖瀚瑰Ο閬嶆晸閿燂拷");
 			request.getRequestDispatcher("error.jsp")
 					.forward(request, response);
 		} else {
-			request.setAttribute("error", "鍥鹃敓鏂ゆ嫹閿熸枻鎷锋伅閿熸枻鎷烽敓缁炑嶆嫹鍩介敓锟�");
+			request.setAttribute("error", "閸ラ箖鏁撻弬銈嗗闁跨喐鏋婚幏閿嬩紖闁跨喐鏋婚幏鐑芥晸缂佺倯宥嗗閸╀粙鏁撻敓锟�");
 			request.getRequestDispatcher("error.jsp")
 					.forward(request, response);
 		}
 	}
 
-	/*********************** 閿熸枻鎷疯鍏ㄩ敓鏂ゆ嫹鍥鹃敓鏂ゆ嫹閿熸枻鎷锋伅 **************************/
+	/*********************** 闁跨喐鏋婚幏鐤嚄閸忋劑鏁撻弬銈嗗閸ラ箖鏁撻弬銈嗗闁跨喐鏋婚幏閿嬩紖 **************************/
 	private void bookQuery(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		String str = null;
-		request.setAttribute("book", bookDAO.query(str)); // 閿熸枻鎷烽敓鏂ゆ嫹璇㈤敓鏂ゆ嫹閿熸枻鎷烽敓鏂ゆ嫹濞寸禉ook閿熸枻鎷�
+		request.setAttribute("book", bookDAO.query(str)); // 闁跨喐鏋婚幏鐑芥晸閺傘倖瀚圭拠銏ゆ晸閺傘倖瀚归柨鐔告灮閹风兘鏁撻弬銈嗗婵炲绂塷ok闁跨喐鏋婚幏锟�
 		request.getRequestDispatcher("book.jsp").forward(request, response);
 	}
 
-	/*********************** 閿熸枻鎷烽敓鏂ゆ嫹閿熸枻鎷疯鍥鹃敓鏂ゆ嫹閿熸枻鎷锋伅 **************************/
+	/*********************** 闁跨喐鏋婚幏鐑芥晸閺傘倖瀚归柨鐔告灮閹风柉顕楅崶楣冩晸閺傘倖瀚归柨鐔告灮閹烽攱浼� **************************/
 	private void bookifQuery(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		String str = null;
@@ -92,22 +92,22 @@ public class Book extends HttpServlet {
 					+ request.getParameter("key") + "%";
 		}
 		request.setAttribute("ifbook", bookDAO.query(str));
-		System.out.print("閿熸枻鎷烽敓鏂ゆ嫹閿熸枻鎷疯鍥鹃敓鏂ゆ嫹閿熸枻鎷锋伅鏃堕敓鏂ゆ嫹str:" + str);
+		System.out.print("闁跨喐鏋婚幏鐑芥晸閺傘倖瀚归柨鐔告灮閹风柉顕楅崶楣冩晸閺傘倖瀚归柨鐔告灮閹烽攱浼呴弮鍫曟晸閺傘倖瀚箂tr:" + str);
 		request.getRequestDispatcher("bookQuery.jsp").forward(request, response);
 	}
 
-	/*********************** 閿熸枻鎷疯閿熺潾闈╂嫹鍥鹃敓鏂ゆ嫹閿熸枻鎷锋伅 **************************/
+	/*********************** 闁跨喐鏋婚幏鐤嚄闁跨喓娼鹃棃鈺傚閸ラ箖鏁撻弬銈嗗闁跨喐鏋婚幏閿嬩紖 **************************/
 	private void bookModifyQuery(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		BookForm bookForm = new BookForm();
-		System.out.println("閿熸枻鎷疯閿熺潾闈╂嫹鍥鹃敓鏂ゆ嫹閿熸枻鎷锋伅閿熸枻鎷�" + request.getParameter("ID"));
+		System.out.println("闁跨喐鏋婚幏鐤嚄闁跨喓娼鹃棃鈺傚閸ラ箖鏁撻弬銈嗗闁跨喐鏋婚幏閿嬩紖闁跨喐鏋婚幏锟�" + request.getParameter("ID"));
 		bookForm.setId(Integer.valueOf(request.getParameter("ID")));
 		request.setAttribute("bookQueryif", bookDAO.queryM(bookForm));
 		request.getRequestDispatcher("book_Modify.jsp").forward(request,
 				response);
 	}
 
-	/*********************** 閿熸枻鎷疯鍥鹃敓鏂ゆ嫹閿熸枻鎷风粏閿熸枻鎷锋伅 **************************/
+	/*********************** 闁跨喐鏋婚幏鐤嚄閸ラ箖鏁撻弬銈嗗闁跨喐鏋婚幏椋庣矎闁跨喐鏋婚幏閿嬩紖 **************************/
 	private void bookDetail(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		BookForm bookForm = new BookForm();
@@ -117,12 +117,12 @@ public class Book extends HttpServlet {
 				response);
 	}
 
-	/*********************** 閿熺潾闈╂嫹鍥鹃敓鏂ゆ嫹閿熸枻鎷锋伅 **************************/
+	/*********************** 闁跨喓娼鹃棃鈺傚閸ラ箖鏁撻弬銈嗗闁跨喐鏋婚幏閿嬩紖 **************************/
 	private void bookModify(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		BookForm bookForm = new BookForm(); // 瀹為敓鏂ゆ嫹閿熸枻鎷稡ookForm閿熸枻鎷�
+		BookForm bookForm = new BookForm(); // 鐎圭偤鏁撻弬銈嗗闁跨喐鏋婚幏绋okForm闁跨喐鏋婚幏锟�
 		bookForm.setId(Integer.parseInt(request.getParameter("id")));
-		bookForm.setBarcode(request.getParameter("barcode")); // 閿熸枻鎷峰彇閿熸枻鎷烽敓鏂ゆ嫹閿熸枻鎷烽敓鏂ゆ嫹閿熸枻鎷烽敓鏂ゆ嫹閿熸枻鎷烽敓鏂ゆ嫹
+		bookForm.setBarcode(request.getParameter("barcode")); // 闁跨喐鏋婚幏宄板絿闁跨喐鏋婚幏鐑芥晸閺傘倖瀚归柨鐔告灮閹风兘鏁撻弬銈嗗闁跨喐鏋婚幏鐑芥晸閺傘倖瀚归柨鐔告灮閹风兘鏁撻弬銈嗗
 		bookForm.setBookName(request.getParameter("bookName"));
 		bookForm.setTypeId(Integer.parseInt(request.getParameter("typeId")));
 		bookForm.setAuthor(request.getParameter("author"));
@@ -133,25 +133,25 @@ public class Book extends HttpServlet {
 		bookForm.setBookcaseid(Integer.parseInt(request
 				.getParameter("bookcaseid")));
 		bookForm.setOperator(request.getParameter("operator"));
-		int ret = bookDAO.update(bookForm); // 閿熸枻鎷烽敓鏂ゆ嫹閿熺潾闈╂嫹鍥鹃敓鏂ゆ嫹閿熸枻鎷锋伅閿熶茎鍑ゆ嫹閿熸枻鎷穟pdate()
+		int ret = bookDAO.update(bookForm); // 闁跨喐鏋婚幏鐑芥晸閺傘倖瀚归柨鐔烘骄闂堚晜瀚归崶楣冩晸閺傘倖瀚归柨鐔告灮閹烽攱浼呴柨鐔惰寧閸戙倖瀚归柨鐔告灮閹风pdate()
 		if (ret == 0) {
-			request.setAttribute("error", "閿熺潾闈╂嫹鍥鹃敓鏂ゆ嫹閿熸枻鎷锋伅澶遍敓鏉帮綇鎷�");
+			request.setAttribute("error", "闁跨喓娼鹃棃鈺傚閸ラ箖鏁撻弬銈嗗闁跨喐鏋婚幏閿嬩紖婢堕亶鏁撻弶甯秶閹凤拷");
 			request.getRequestDispatcher("error.jsp")
-					.forward(request, response); // 杞敓鏂ゆ嫹閿熸枻鎷烽敓鏂ゆ嫹閿熸枻鎷风ず椤甸敓鏂ゆ嫹
+					.forward(request, response); // 鏉烆剟鏁撻弬銈嗗闁跨喐鏋婚幏鐑芥晸閺傘倖瀚归柨鐔告灮閹烽銇氭い鐢告晸閺傘倖瀚�
 		} else {
 			request.getRequestDispatcher("book_ok.jsp?para=2").forward(request,
-					response); // 杞敓鏂ゆ嫹閿熺潾鏀规垚鐧告嫹椤甸敓鏂ゆ嫹
+					response); // 鏉烆剟鏁撻弬銈嗗闁跨喓娼鹃弨瑙勫灇閻у憡瀚规い鐢告晸閺傘倖瀚�
 		}
 	}
 
-	/*********************** 鍒犻敓鏂ゆ嫹鍥鹃敓鏂ゆ嫹閿熸枻鎷锋伅 **************************/
+	/*********************** 閸掔娀鏁撻弬銈嗗閸ラ箖鏁撻弬銈嗗闁跨喐鏋婚幏閿嬩紖 **************************/
 	private void bookDel(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		BookForm bookForm = new BookForm();
 		bookForm.setId(Integer.valueOf(request.getParameter("ID")));
 		int ret = bookDAO.delete(bookForm);
 		if (ret == 0) {
-			request.setAttribute("error", "鍒犻敓鏂ゆ嫹鍥鹃敓鏂ゆ嫹閿熸枻鎷锋伅澶遍敓鏉帮綇鎷�");
+			request.setAttribute("error", "閸掔娀鏁撻弬銈嗗閸ラ箖鏁撻弬銈嗗闁跨喐鏋婚幏閿嬩紖婢堕亶鏁撻弶甯秶閹凤拷");
 			request.getRequestDispatcher("error.jsp")
 					.forward(request, response);
 		} else {
