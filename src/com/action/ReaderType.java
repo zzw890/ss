0f5d3d47fb519598dbce2aa128e200e987edfa26
wangxiufeng <wangxiufeng@infohold.com.cn>
2019-06-30 18:42:06 +0800

    tj

--- src/com/action/ReaderType.java
+++ src/com/action/ReaderType.java
@@ -147,6 +147,6 @@ public class ReaderType extends HttpServlet {
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		doGet(request, response);//zzwws
+		doGet(request, response);//zzwzzw
 	}
 }//wswsws
